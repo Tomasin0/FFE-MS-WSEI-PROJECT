884bba598e597087a08abd1b1776aae1f620e199
Tomasin0 <tomax12613@gmail.com>
2022-10-19 18:07:02 +0200

    Added styleHelpers & Common components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,27 @@
 import { FC } from 'react';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import {  Home } from './components/Routes/Home/home';
 import { Posts } from './components/Routes/Posts/posts';
+import { GlobalStyle } from './styleHelpers/global';
+import { Layout } from './components/Common/Layout';
 
 export const App: FC = () => {
   return (
+    <GlobalStyle />
     <BrowserRouter>
-      <img src={`${process.env.PUBLIC_URL}/img/logo.png`}
-      <div>
-      
-      </div>
+        <Layout>
+            <Routes>
+                <Route path="posts" element={<Posts />} />
+                <Route index element={<Home />} />
+            </Routes>
+        </Layout>
     </BrowserRouter>
-  )
-}
+
+    //   <img src={`${process.env.PUBLIC_URL}/img/logo.png`}
+    //   <div>
+      
+    //   </div>
+    // </BrowserRouter>
+  );
+};
